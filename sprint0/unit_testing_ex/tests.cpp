fcc799a19ce7f93a0d749dccc4de32fb3f249a2a
John C <jwcgnq@umkc.edu>
2024-08-21 13:17:22 -0500

    Added comments and code complies more with standards

--- sprint0/unit_testing_ex/tests.cpp
+++ sprint0/unit_testing_ex/tests.cpp
@@ -9,6 +9,8 @@ TEST_CASE( "Numbers, 2 and 2 are added", "[numbers::add]") {
     REQUIRE( test1.add() == 4);
 }
 
+/* This one will fail since the function accidently
+   adds the numbers instead of subtracting */
 TEST_CASE( "Numbers, 2 and 2 are subtracted", "[numbers::add]") {
     numbers test1;
     test1.number1 = 2;
