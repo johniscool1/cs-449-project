85e2f3d2c06819310bdab5f19bb1a6fbb2ac4333
John C <jwcgnq@umkc.edu>
2024-08-21 13:21:42 -0500

    put source in src dir and changed cmakelist to go with it

--- sprint0/unit_testing_ex/src/tests.cpp
+++ sprint0/unit_testing_ex/src/tests.cpp
@@ -2,7 +2,8 @@
 #include "catch_amalgamated.hpp"
 #include "functions.hpp"
 
-TEST_CASE( "Numbers, 2 and 2 are added", "[numbers::add]") {
+TEST_CASE( "Numbers, 2 and 2 are added", "[numbers::add]") 
+{
     numbers test1;
     test1.number1 = 2;
     test1.number2 = 2;
@@ -11,7 +12,8 @@ TEST_CASE( "Numbers, 2 and 2 are added", "[numbers::add]") {
 
 /* This one will fail since the function accidently
    adds the numbers instead of subtracting */
-TEST_CASE( "Numbers, 2 and 2 are subtracted", "[numbers::add]") {
+TEST_CASE( "Numbers, 2 and 2 are subtracted", "[numbers::add]") 
+{
     numbers test1;
     test1.number1 = 2;
     test1.number2 = 2;
