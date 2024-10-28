645ea013f7af4a30720cfc66598ec9b8bd76e22a
John C <jwcgnq@umkc.edu>
2024-10-28 12:21:38 -0500

    fixed sequencefinder bug

--- sprint3/src/game_logic.cpp
+++ sprint3/src/game_logic.cpp
@@ -93,13 +93,13 @@ int GameLogic::SequenceFinder(int rows, int cols, PlayerLogic* Player1Data, Play
     return a.y < b.y;
   });
   //Find Sequences
-
+  //cout << "Size " << TempVect.size() << endl;
   for(int f = 0; f < TempVect.size(); f++)
   {
 
     int k = f;
    if(TempVect[k].piece == 1 && TempVect.size() >= 3){
-     //printf("Found S at %d,%d", TempVect[k].x, TempVect[k].y);
+     printf("Found S at %d,%d\n", TempVect[k].x + 1, TempVect[k].y + 1);
      k++;
      if(TempVect[k].piece == 0 && k < TempVect.size() && TempVect[k].y == TempVect[k-1].y + 1)
      {
@@ -108,7 +108,7 @@ int GameLogic::SequenceFinder(int rows, int cols, PlayerLogic* Player1Data, Play
       if(TempVect[k].piece == 1 && k < TempVect.size()&& TempVect[k].y == TempVect[k-1].y + 1)
       {
       //printf("Found S at %d,%d", TempVect[k].x, TempVect[k].y);
-        //printf("Found a Sequence\n");
+        printf("Found a Sequence n to s\n");
        //check if it has been scored already using a nand gate
       //cout << "1 " << SpacesPlayed[TempVect[k].originalIndex].Scored << " 2 " << SpacesPlayed[TempVect[k-1].originalIndex].Scored << " 3 " << SpacesPlayed[TempVect[k-2].originalIndex].Scored << endl;
        if (!(SpacesPlayed[TempVect[k].originalIndex].Scored && SpacesPlayed[TempVect[k-1].originalIndex].Scored && SpacesPlayed[TempVect[k-2].originalIndex].Scored))
@@ -151,9 +151,9 @@ int GameLogic::SequenceFinder(int rows, int cols, PlayerLogic* Player1Data, Play
       }
      }
     }
-    TempVect.clear();
-   }
 
+   }
+  TempVect.clear();
   }
   TempVect.clear();
 
@@ -187,12 +187,13 @@ for(int j = 0; j < rows; j++) {
   {
     return a.x < b.x;
   });
+  //cout << "Size " << TempVect.size() << endl;
   for(int d = 0; d < TempVect.size(); d++)
   {
     int k = d;
 
    if(TempVect[k].piece == 1 && TempVect.size() >= 3){
-     //printf("Found S at %d,%d", TempVect[k].x, TempVect[k].y);
+     printf("Found S at %d,%d\n", TempVect[k].x +1, TempVect[k].y+1);
      k++;
      if(TempVect[k].piece == 0 && k < TempVect.size() && TempVect[k].x == TempVect[k-1].x + 1)
      {
@@ -201,7 +202,7 @@ for(int j = 0; j < rows; j++) {
       if(TempVect[k].piece == 1 && k < TempVect.size() && TempVect[k].x == TempVect[k-1].x + 1)
       {
        //printf("Found S at %d,%d", TempVect[k].x, TempVect[k].y);
-       //printf("Found a Sequence\n");
+       printf("Found a Sequence e to w\n");
        //check if it has been scored already using a nand gate
        if (!(SpacesPlayed[TempVect[k].originalIndex].Scored && SpacesPlayed[TempVect[k-1].originalIndex].Scored && SpacesPlayed[TempVect[k-2].originalIndex].Scored))
        {
@@ -241,9 +242,9 @@ for(int j = 0; j < rows; j++) {
       }
      }
     }
-    TempVect.clear();
-   }
 
+   }
+    TempVect.clear();
   }
 
   TempVect.clear();
@@ -258,7 +259,7 @@ for(int j = 0; j < rows; j++) {
 
 
 
-//check SW to NW
+//check SW to NE
 
 
   for(int i = 0; i < cols; i++)
@@ -290,6 +291,7 @@ for(int j = 0; j < rows; j++) {
   {
     return a.x < b.x;
   });
+
   for(int d = 0; d < TempVect.size(); d++)
   {
    int k = d;
@@ -346,34 +348,39 @@ for(int j = 0; j < rows; j++) {
   TempVect.clear();
   }
   }
+
+
   //check After the middle
 
-  for(int i = cols; i > 0; i--)
+
+
+  for(int i = 1; i < cols; i++)
   {
     int counter = i;
-    for (int j = 1; j < rows; j++){
-      //cout << "X: " << j << "Y: " << counter << endl;
+    for(int j = rows; j > 0; j--)
+    {
+      //cout << "X: " << counter << "Y: " << j << endl;
       for(int l = 0; l < SpacesPlayed.size(); l++)
      {
-   if(SpacesPlayed[l].y == counter && SpacesPlayed[l].x == j)
-   {
-     //create temp var
-     //cout << "ADDED " << SpacesPlayed[k].x << SpacesPlayed[k].y <<  endl;
-     tempFilledSpace TempSpaceData;
-     TempSpaceData.x = SpacesPlayed[l].x;
-     TempSpaceData.y = SpacesPlayed[l].y;
-     TempSpaceData.piece = SpacesPlayed[l].piece;
-     TempSpaceData.player = SpacesPlayed[l].player;
-     TempSpaceData.Scored = SpacesPlayed[l].Scored;
-     TempSpaceData.Button = SpacesPlayed[l].Button;
-     TempSpaceData.originalIndex = l;
-     TempVect.push_back(TempSpaceData);
-   }
+        if(SpacesPlayed[l].y == counter && SpacesPlayed[l].x == j)
+        {
+          //create temp var
+          //cout << "ADDED " << SpacesPlayed[k].x << SpacesPlayed[k].y <<  endl;
+          tempFilledSpace TempSpaceData;
+          TempSpaceData.x = SpacesPlayed[l].x;
+          TempSpaceData.y = SpacesPlayed[l].y;
+          TempSpaceData.piece = SpacesPlayed[l].piece;
+          TempSpaceData.player = SpacesPlayed[l].player;
+          TempSpaceData.Scored = SpacesPlayed[l].Scored;
+          TempSpaceData.Button = SpacesPlayed[l].Button;
+          TempSpaceData.originalIndex = l;
+          TempVect.push_back(TempSpaceData);
+        }
 
     }
-    counter = counter -1;
-  }
-  sort(TempVect.begin(), TempVect.end(), [](const tempFilledSpace& a, const tempFilledSpace& b)
+      counter++;
+    }
+    sort(TempVect.begin(), TempVect.end(), [](const tempFilledSpace& a, const tempFilledSpace& b)
   {
     return a.x < b.x;
   });
@@ -389,7 +396,7 @@ for(int j = 0; j < rows; j++) {
       k++;
       if(TempVect[k].piece == 1 && k < TempVect.size() && TempVect[k].x -1 == TempVect[k-1].x)
       {
-      //printf("Found S at %d,%d\n", TempVect[k].x, TempVect[k].y);
+     // printf("Found S at %d,%d\n", TempVect[k].x, TempVect[k].y);
       //printf("Found a Sequence\n");
        //check if it has been scored already using a nand gate
        if (!(SpacesPlayed[TempVect[k].originalIndex].Scored && SpacesPlayed[TempVect[k-1].originalIndex].Scored && SpacesPlayed[TempVect[k-2].originalIndex].Scored))
@@ -434,6 +441,100 @@ for(int j = 0; j < rows; j++) {
   }
   }
 
+  /*
+  int counter = cols;
+
+  for(int i = cols; i > 0; i--)
+  {
+
+    for (int j = 1; j < rows; j++){
+      cout << "X: " << j << "Y: " << counter << endl;
+      for(int l = 0; l < SpacesPlayed.size(); l++)
+     {
+   if(SpacesPlayed[l].y == counter && SpacesPlayed[l].x == j)
+   {
+     //create temp var
+     //cout << "ADDED " << SpacesPlayed[k].x << SpacesPlayed[k].y <<  endl;
+     tempFilledSpace TempSpaceData;
+     TempSpaceData.x = SpacesPlayed[l].x;
+     TempSpaceData.y = SpacesPlayed[l].y;
+     TempSpaceData.piece = SpacesPlayed[l].piece;
+     TempSpaceData.player = SpacesPlayed[l].player;
+     TempSpaceData.Scored = SpacesPlayed[l].Scored;
+     TempSpaceData.Button = SpacesPlayed[l].Button;
+     TempSpaceData.originalIndex = l;
+     TempVect.push_back(TempSpaceData);
+   }
+
+    }
+
+    counter = counter -1;
+
+  }
+  counter = cols;
+
+  cout << "--------------" << endl;
+  sort(TempVect.begin(), TempVect.end(), [](const tempFilledSpace& a, const tempFilledSpace& b)
+  {
+    return a.x < b.x;
+  });
+  for(int d = 0; d < TempVect.size(); d++)
+  {
+   int k = d;
+   if(TempVect[k].piece == 1 && TempVect.size() >= 3){
+     //printf("Found S at %d,%d\n", TempVect[k].x, TempVect[k].y);
+     k++;
+     if(TempVect[k].piece == 0 && k < TempVect.size() && TempVect[k].x -1 == TempVect[k-1].x)
+     {
+      //printf("Found O at %d,%d\n", TempVect[k].x, TempVect[k].y);
+      k++;
+      if(TempVect[k].piece == 1 && k < TempVect.size() && TempVect[k].x -1 == TempVect[k-1].x)
+      {
+     // printf("Found S at %d,%d\n", TempVect[k].x, TempVect[k].y);
+      //printf("Found a Sequence\n");
+       //check if it has been scored already using a nand gate
+       if (!(SpacesPlayed[TempVect[k].originalIndex].Scored && SpacesPlayed[TempVect[k-1].originalIndex].Scored && SpacesPlayed[TempVect[k-2].originalIndex].Scored))
+       {
+       //printf("Added point to Player %d\n", CurrentTurn);
+       //Mark the spaces as being scored already
+       SpacesPlayed[TempVect[k].originalIndex].Scored = true;
+       SpacesPlayed[TempVect[k-1].originalIndex].Scored = true;
+       SpacesPlayed[TempVect[k-2].originalIndex].Scored = true;
+       //add the sequences to this vector to later indiacte who scored
+       FoundSequences.push_back(TempVect[k]);
+       FoundSequences.push_back(TempVect[k-1]);
+       FoundSequences.push_back(TempVect[k-2]);
+        //subtract 1 from score becuase we run this after the turn has changed
+       switch (CurrentTurn - 1)
+       {
+         case 1:
+          Player1Data->addPoint();
+          if( Last_Player_Scored == 0)
+          {
+            Last_Player_Scored = 11;
+          } else {
+              Last_Player_Scored++;
+          }
+          break;
+         case 0: // since we are subtracting one, player 2 is now 0
+          Player2Data->addPoint();
+          if( Last_Player_Scored == 0)
+          {
+            Last_Player_Scored = 21;
+          } else {
+            Last_Player_Scored++;
+          }
+          break;
+       }
+       //printf("\n-------------------------\n");
+      }
+      }
+     }
+   }
+  TempVect.clear();
+  }
+  }
+  */
 
 
 
@@ -445,7 +546,7 @@ for(int j = 0; j < rows; j++) {
 
  TempVect.clear();
 
- //check SE to NE
+ //check SE to NW
 
   for(int i = 0; i < cols; i++)
   {
